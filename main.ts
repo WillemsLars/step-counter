0eee7f5fd690f909d8b747df8be14664b835f87e
WillemsLars <121558591+WillemsLars@users.noreply.github.com>
2023-01-03 11:45:30 +0100

    leuke spelletjes

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
-input.onGesture(Gesture.Shake, function () {
-    stap += 1
-    led.stopAnimation()
-})
-let stap = 0
-basic.forever(function () {
-    basic.showNumber(stap)
+let rendom_numer = 0
+input.onButtonPressed(Button.A, function () {
+    rendom_numer = randint(1, 6)
+    if (rendom_numer == 1) {
+        basic.showString("OMTER EERST EEN BOTLE FLIP")
+    } else if (rendom_numer == 2) {
+        basic.showString("PING PONG SCHOT")
+    } else if (rendom_numer == 3) {
+        basic.showString("IN DE BUL GOJEN")
+    } else if (rendom_numer == 4) {
+        basic.showString("OMTER EERST 6 GOOIEN")
+    } else if (rendom_numer == 5) {
+        basic.showString("BALON WETSTRIJD")
+    } else if (rendom_numer == 6) {
+        basic.showString("DRINK OMTER EERST EEN BEKER UIT")
+    }
 })
